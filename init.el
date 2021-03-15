d6690c8570868b5c6b6967c978eb1dc381d8f117
RAZAFIMAHEFA Aina <nasoloaina@gmail.com>
2021-03-15 10:43:56 +0100

    Remove milkybox (unknown) melpa package

--- init.el
+++ init.el
@@ -7,8 +7,6 @@
 (add-to-list 'package-archives
              '("tromey" . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives
              '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 
 (add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
